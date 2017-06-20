d6e0fa88798216af18b7c0baf4a949bff1a68327
Sven Speckmaier <sps@ipunkt.biz>
2017-06-20 12:12:32 +0200

    added more commentation to the config file

--- healthcheck/config/healthcheck.php
+++ healthcheck/config/healthcheck.php
@@ -1,5 +1,14 @@
 <?php
 return [
+
+	/**
+	 * Set to false to disable the healthcheck route
+	 */
+	'enable' => true,
+
+	/**
+	 * The GET route where the healthcheck is performed
+	 */
 	'route' => '/healthcheck',
 
 	/**
@@ -14,10 +23,20 @@ return [
 		'storage'
 	],
 
+	/**
+	 * Database options:
+	 *
+	 * Database => tablename.
+	 * Tested by doing a select limit 1 on
+	 */
 	'database' => [
 		'dbname' => 'dbtable',
 	],
 
+	/**
+	 * Array of pathes to check for write access
+	 * Tested by trying to write the current date to PATH/healthcheck.txt
+	 */
 	'storage' => [
 		storage_path()
 	],
