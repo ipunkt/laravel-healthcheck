b5214de7e63289d41d78cef04c34d97c849d7c7a
Robert Kummer <rok@ipunkt.biz>
2017-07-10 15:42:53 +0200

    Solr checker added

--- healthcheck/checkers.php
+++ healthcheck/checkers.php
@@ -3,4 +3,5 @@ return [
 	'Ipunkt\LaravelHealthcheck\Database\DatabaseHealthcheckProvider',
 	'Ipunkt\LaravelHealthcheck\Storage\StorageHealthcheckProvider',
 	'Ipunkt\LaravelHealthcheck\Redis\RedisHealthcheckProvider',
+    'Ipunkt\LaravelHealthcheck\Solr\SolrHealthcheckProvider',
 ];
